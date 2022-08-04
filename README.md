2d9230fe548a7b993759ab137af6193ce0d41c45
pabasara-mahindapala <pabasaramahindapala@gmail.com>
2022-08-04 14:06:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-9fv7bs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-9fv7bs)
---
+++ src/app/app.component.ts
@@ -0,0 +1,338 @@
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  Output,
+  EventEmitter,
+  Input,
+} from '@angular/core';
+import { fromEvent, of } from 'rxjs';
+import { debounceTime, filter, pluck, switchMap, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  @ViewChild('search', { static: false }) searchInput: ElementRef;
+  @Input() mapFunction: any;
+  @Input() initialDisplay: any;
+  @Input() isRequired: any;
+  @Input() minLength: number = 1;
+  @Input() disabled: boolean;
+  countryList = [
+    'Afghanistan',
+    'Albania',
+    'Algeria',
+    'American Samoa',
+    'Andorra',
+    'Angola',
+    'Anguilla',
+    'Antarctica',
+    'Antigua and Barbuda',
+    'Argentina',
+    'Armenia',
+    'Aruba',
+    'Australia',
+    'Austria',
+    'Azerbaijan',
+    'Bahamas (the)',
+    'Bahrain',
+    'Bangladesh',
+    'Barbados',
+    'Belarus',
+    'Belgium',
+    'Belize',
+    'Benin',
+    'Bermuda',
+    'Bhutan',
+    'Bolivia (Plurinational State of)',
+    'Bonaire, Sint Eustatius and Saba',
+    'Bosnia and Herzegovina',
+    'Botswana',
+    'Bouvet Island',
+    'Brazil',
+    'British Indian Ocean Territory (the)',
+    'Brunei Darussalam',
+    'Bulgaria',
+    'Burkina Faso',
+    'Burundi',
+    'Cabo Verde',
+    'Cambodia',
+    'Cameroon',
+    'Canada',
+    'Cayman Islands (the)',
+    'Central African Republic (the)',
+    'Chad',
+    'Chile',
+    'China',
+    'Christmas Island',
+    'Cocos (Keeling) Islands (the)',
+    'Colombia',
+    'Comoros (the)',
+    'Congo (the Democratic Republic of the)',
+    'Congo (the)',
+    'Cook Islands (the)',
+    'Costa Rica',
+    'Croatia',
+    'Cuba',
+    'Curaçao',
+    'Cyprus',
+    'Czechia',
+    "Côte d'Ivoire",
+    'Denmark',
+    'Djibouti',
+    'Dominica',
+    'Dominican Republic (the)',
+    'Ecuador',
+    'Egypt',
+    'El Salvador',
+    'Equatorial Guinea',
+    'Eritrea',
+    'Estonia',
+    'Eswatini',
+    'Ethiopia',
+    'Falkland Islands (the) [Malvinas]',
+    'Faroe Islands (the)',
+    'Fiji',
+    'Finland',
+    'France',
+    'French Guiana',
+    'French Polynesia',
+    'French Southern Territories (the)',
+    'Gabon',
+    'Gambia (the)',
+    'Georgia',
+    'Germany',
+    'Ghana',
+    'Gibraltar',
+    'Greece',
+    'Greenland',
+    'Grenada',
+    'Guadeloupe',
+    'Guam',
+    'Guatemala',
+    'Guernsey',
+    'Guinea',
+    'Guinea-Bissau',
+    'Guyana',
+    'Haiti',
+    'Heard Island and McDonald Islands',
+    'Holy See (the)',
+    'Honduras',
+    'Hong Kong',
+    'Hungary',
+    'Iceland',
+    'India',
+    'Indonesia',
+    'Iran (Islamic Republic of)',
+    'Iraq',
+    'Ireland',
+    'Isle of Man',
+    'Israel',
+    'Italy',
+    'Jamaica',
+    'Japan',
+    'Jersey',
+    'Jordan',
+    'Kazakhstan',
+    'Kenya',
+    'Kiribati',
+    "Korea (the Democratic People's Republic of)",
+    'Korea (the Republic of)',
+    'Kuwait',
+    'Kyrgyzstan',
+    "Lao People's Democratic Republic (the)",
+    'Latvia',
+    'Lebanon',
+    'Lesotho',
+    'Liberia',
+    'Libya',
+    'Liechtenstein',
+    'Lithuania',
+    'Luxembourg',
+    'Macao',
+    'Madagascar',
+    'Malawi',
+    'Malaysia',
+    'Maldives',
+    'Mali',
+    'Malta',
+    'Marshall Islands (the)',
+    'Martinique',
+    'Mauritania',
+    'Mauritius',
+    'Mayotte',
+    'Mexico',
+    'Micronesia (Federated States of)',
+    'Moldova (the Republic of)',
+    'Monaco',
+    'Mongolia',
+    'Montenegro',
+    'Montserrat',
+    'Morocco',
+    'Mozambique',
+    'Myanmar',
+    'Namibia',
+    'Nauru',
+    'Nepal',
+    'Netherlands (the)',
+    'New Caledonia',
+    'New Zealand',
+    'Nicaragua',
+    'Niger (the)',
+    'Nigeria',
+    'Niue',
+    'Norfolk Island',
+    'Northern Mariana Islands (the)',
+    'Norway',
+    'Oman',
+    'Pakistan',
+    'Palau',
+    'Palestine, State of',
+    'Panama',
+    'Papua New Guinea',
+    'Paraguay',
+    'Peru',
+    'Philippines (the)',
+    'Pitcairn',
+    'Poland',
+    'Portugal',
+    'Puerto Rico',
+    'Qatar',
+    'Republic of North Macedonia',
+    'Romania',
+    'Russian Federation (the)',
+    'Rwanda',
+    'Réunion',
+    'Saint Barthélemy',
+    'Saint Helena, Ascension and Tristan da Cunha',
+    'Saint Kitts and Nevis',
+    'Saint Lucia',
+    'Saint Martin (French part)',
+    'Saint Pierre and Miquelon',
+    'Saint Vincent and the Grenadines',
+    'Samoa',
+    'San Marino',
+    'Sao Tome and Principe',
+    'Saudi Arabia',
+    'Senegal',
+    'Serbia',
+    'Seychelles',
+    'Sierra Leone',
+    'Singapore',
+    'Sint Maarten (Dutch part)',
+    'Slovakia',
+    'Slovenia',
+    'Solomon Islands',
+    'Somalia',
+    'South Africa',
+    'South Georgia and the South Sandwich Islands',
+    'South Sudan',
+    'Spain',
+    'Sri Lanka',
+    'Sudan (the)',
+    'Suriname',
+    'Svalbard and Jan Mayen',
+    'Sweden',
+    'Switzerland',
+    'Syrian Arab Republic',
+    'Taiwan',
+    'Tajikistan',
+    'Tanzania, United Republic of',
+    'Thailand',
+    'Timor-Leste',
+    'Togo',
+    'Tokelau',
+    'Tonga',
+    'Trinidad and Tobago',
+    'Tunisia',
+    'Turkey',
+    'Turkmenistan',
+    'Turks and Caicos Islands (the)',
+    'Tuvalu',
+    'Uganda',
+    'Ukraine',
+    'United Arab Emirates (the)',
+    'United Kingdom of Great Britain and Northern Ireland (the)',
+    'United States Minor Outlying Islands (the)',
+    'United States of America (the)',
+    'Uruguay',
+    'Uzbekistan',
+    'Vanuatu',
+    'Venezuela (Bolivarian Republic of)',
+    'Viet Nam',
+    'Virgin Islands (British)',
+    'Virgin Islands (U.S.)',
+    'Wallis and Futuna',
+    'Western Sahara',
+    'Yemen',
+    'Zambia',
+    'Zimbabwe',
+    'Åland Islands',
+  ];
+
+  showList: boolean = false;
+  showError: boolean = false;
+  isBlurred: boolean = false;
+  resultList = [];
+  listId: string;
+
+  ngAfterViewInit() {
+    fromEvent(this.searchInput.nativeElement, 'input')
+      .pipe(
+        pluck('target', 'value'),
+        filter((searchTerm: string) => {
+          return (
+            searchTerm.trim().length >= (this.minLength ? this.minLength : 1)
+          );
+        }),
+        debounceTime(500),
+        tap((_) => {
+          this.showList = true;
+        }),
+        switchMap((changedValue) => {
+          return this.searchRequest(changedValue);
+        })
+      )
+      .subscribe((result: any) => {
+        this.resultList = result;
+      });
+  }
+
+  searchRequest(changedValue: string): any {
+    return of(
+      this.countryList.filter((x) =>
+        x.toLowerCase().includes(changedValue.toLowerCase())
+      )
+    );
+  }
+
+  handleSelected(value: any) {
+    if (value) {
+      this.searchInput.nativeElement.blur();
+      this.searchInput.nativeElement.value = value;
+      this.initialDisplay = this.searchInput.nativeElement.value;
+      this.showList = false;
+    }
+  }
+
+  selectFirst() {
+    if (this.resultList.length > 0) {
+      this.handleSelected(this.resultList[0].displayName);
+    }
+  }
+
+  clearText() {
+    this.searchInput.nativeElement.value = '';
+  }
+
+  showErrorMessage() {
+    this.showError = true;
+  }
+
+  hideErrorMessage() {
+    this.showError = false;
+  }
+}
